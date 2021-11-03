0893d9153a309f47418668faa61bf427c2edbefe
Gudgeon, Alexander <Alexander.Gudgeon@justice.gov.uk>
2021-11-03 10:48:04 +0000

    commit on main

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'HMCTS - Data Architecture, Strategy & Innovation',
+  serviceName: 'HMCTS - DTS - G6 & G7 Conflict of Interest Register',
 
   // Default port that prototype runs on
   port: '3000',
