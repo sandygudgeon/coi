724d7afa97483c0e05b0466d9301be961eb1a9b1
Sandy Gudgeon <alexander.gudgeon@justice.gov.uk>
2021-11-03 18:46:07 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -6,15 +6,15 @@ const router = express.Router()
 // Run this code when a form is submitted to 'conflict-answer'
 router.post('/conflicty', function (req, res) {
   // Make a variable and give it the value from 'conflict' question page
-  var varconflict = req.session.data['interestsyesno']
+  var varconflict = req.session.data.interestsyesno
   // Check whether the user has selected Y to having a conflict
-  if (varconflict === 'Yes'){
+  if (varconflict === 'Yes') {
     // Send user to next page to record conflict details
     res.redirect('/conflicty')
   } else {
     // Send user to check answers page
     res.redirect('/check-your-answers')
   }
-  })
+})
 
 module.exports = router
